e5e7ab874f87fefcf020bc56f6b0533f46c3c1a6
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-10-06 23:28:42 -0700

    Added simple home page and Raimica map page with map.

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+    echo '
+    <h1>Welcome to the Dm\'s Shields!</h1>
+    <a href="/raimica-map/raimca-map.php">Go to Raimica Map</a>
+    ';
---
+++ raimica-map/raimica-map.php
@@ -0,0 +1,1 @@
+<img src="https://res.cloudinary.com/josh-drentlaw-web-development/image/upload/v1570429648/thedmsshield.com/Rimica_Region.jpg" />
