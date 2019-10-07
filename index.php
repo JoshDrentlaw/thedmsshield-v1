40a3169c150c0ac71c527875a2810a7dacf2afea
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-10-06 23:39:35 -0700

    Fixed link typo.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
     echo '
     <h1>Welcome to the Dm\'s Shields!</h1>
-    <a href="/raimica-map/raimca-map.php">Go to Raimica Map</a>
+    <a href="/raimica-map/raimica-map.php">Go to Raimica Map</a>
     ';
