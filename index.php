f803805cb278fad9f825caced3bba26fae9ff85a
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-10-07 21:22:15 -0700

    Added proper html structure with header and footer files.

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-    echo '
-    <h1>Welcome to the Dm\'s Shields!</h1>
-    <a href="/raimica-map/raimica-map.php">Go to Raimica Map</a>
-    ';
+include 'header.php';
+
+echo '
+<h1>Welcome to the Dm\'s Shields!</h1>
+<a href="/raimica-map/">Go to Raimica Map</a>
+';
+
+include 'footer.php';
---
+++ raimica-map/index.php
@@ -0,0 +1,6 @@
+<?php
+include '../header.php';
+
+echo '<img src="https://res.cloudinary.com/josh-drentlaw-web-development/image/upload/v1570429648/thedmsshield.com/Rimica_Region.jpg" />';
+
+include '../footer.php';
