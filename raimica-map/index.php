46a6d8a34e59beff6e70745ff923011d0403717d
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-10-14 23:57:15 -0700

    changed include paths on raimica map.

--- raimica-map/index.php
+++ raimica-map/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include '../header.php';
+include '/header.php';
     
     $host = 'localhost';
     $user = 'root';
@@ -74,4 +74,4 @@ include '../header.php';
     </section>
 </main>
 
-<?php include '../footer.php'; ?>
+<?php include '/footer.php'; ?>
