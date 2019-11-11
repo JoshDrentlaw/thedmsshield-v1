e600efef7478e7f6f590304de47880b2e56a89df
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-11-10 21:23:10 -0800

    Merge commit

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    echo '
-    <h1>Welcome to the Dm\'s Shields!</h1>
-    <a href="/raimica-map/raimica-map.php">Go to Raimica Map</a>
-    ';
--- raimica-map/raimica-map.php
+++
@@ -1,1 +0,0 @@
-<img src="https://res.cloudinary.com/josh-drentlaw-web-development/image/upload/v1570429648/thedmsshield.com/Rimica_Region.jpg" />
