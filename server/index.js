182031e45d6d13e2c1ec74bc548b6bc1de485fae
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-11-30 09:24:07 -0800

    Solving node_env errors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000
 const dev = process.env.NODE_DEV !== 'production' //true false
 
-const nextApp = next({ dev })
+const nextApp = next({ dev: false })
 const handle = nextApp.getRequestHandler() //part of next config
 const mongoose = require('mongoose')
 
