d3957eb1d174a8059c8b0b3bf98375ba3a063097
Josh Drentlaw <jdrentlaw@gmail.com>
2021-01-15 23:51:04 -0800

    updated all places and show places. Added description.

--- resources/views/components/show-place.blade.php
+++ resources/views/components/show-place.blade.php
@@ -1,14 +1,23 @@
-<div id="place-body">
+<div id="place-body" class="card card-body">
+    @csrf
     <h1 class="card-title">
         <span class="show-place-name<?= $isDm ? ' interactive' : '' ?>" contenteditable="true">
-            @csrf
             {{$place->name}}
         </span>
         @if($place->marker)
             <small class="text-muted">{{$place->marker->map->name}}</small>
         @endif
     </h1>
-    <input id="marker-id" value="{{$place->marker->id}}" type="hidden">
+    <div class="row mb-3">
+        <div class="col-sm-12">
+            <span class="show-place-description<?= $isDm ? ' interactive' : '' ?>" contenteditable="true">
+                {{$place->description}}
+            </span>
+        </div>
+    </div>
+    @if(!$place->markerless)
+        <input id="marker-id" value="{{$place->marker->id}}" type="hidden">
+    @endif
     <input id="place-id" value="{{$place->id}}" type="hidden">
     <div class="show-place-editor-container d-none">
         <span>Last updated: <em class="save-time">{{$lastUpdated->format('c')}}</em></span>
@@ -24,5 +33,8 @@
 </div>
 
 @section('component-scripts')
+    <script>
+        const isDm = {!!$isDm!!}
+    </script>
     <script type="module" src="{{ asset('js/show-place.js') . '?' . time() }}"></script>
 @endsection
