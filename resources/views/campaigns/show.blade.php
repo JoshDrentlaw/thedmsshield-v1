cebd55de124391d3c82de931760c646cec0dc148
Josh Drentlaw <jdrentlaw@gmail.com>
2021-04-06 06:28:15 -0700

    Added title to random table modal.

--- resources/views/campaigns/show.blade.php
+++ resources/views/campaigns/show.blade.php
@@ -317,7 +317,7 @@ if ($campaign->cover_public_id) {
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="random-table-name"></h5>
+                <h5 class="modal-title" id="random-table-name-header"></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
